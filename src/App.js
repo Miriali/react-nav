9b1d522755921a91a82043d4309aa95808ccebd3
Miriali <miriali2907@gmail.com>
2020-11-27 16:23:09 -0400

    react-nav

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar/index';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './pages/index';
+import About from './pages/about';
+import Services from './pages/services';
+import Contact from './pages/contactus';
+import Signup from './pages/signup';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    return (
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/services" component={Services} />
+          <Route path="/contact-us" component={Contact} />
+          <Route path="/sign-up" component={Signup} />
+        </Switch>
+      </Router>
   );
 }
 
-export default App;
+export default App
